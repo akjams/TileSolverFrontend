644059f11aba1c0cd3489b37ba48522af0fe36ba
Austin Kemper <austin.kemper@live.com>
2017-03-28 00:05:27 -0400

    tile fill logic

--- index.js
+++ index.js
@@ -10,44 +10,54 @@ function go() {
 
 
 
-	setupTilegrid(tileGrid);
+	tileGrid = $('#tileGrid');
 	var dataArray = getDataArray();
-	setTileGridData(tileGrid, dataArray);
+	drawTiles(tileGrid, dataArray);
 
 
 
-
-	function setupTilegrid(tileGrid) {
-		for (var i = 0; i < SIZE; i++) {
-			appendRow(tileGrid);
-		}
-		
-		function appendRow(tileGrid) {
-			var row = $('<div/>', {
-				class: 'row'
-			});
-			for (var j = 0; j < SIZE; j++) {
-				$('<div/>', {
-				    class: 'col-md-4 gridcell',
-				    text: 'cell'
-				}).appendTo(row);
-			}
-			$(tileGrid).append(row);
-		}
-	}
-
+	/* ********** HELPERS ********** */
 	function getDataArray() {
 		return [1, 2, 3, 4, 5, 6, 7, 8, 0];
 	}
 
-	function setTileGridData(tileGrid, array) {
-		var i = 0
-		$(tileGrid).find(".gridcell").each(function() {
-			$(this).text(array[i]);
-			i++;
-		});
+	function drawTiles(tileGrid, dataArray) {
+
+		$(tileGrid).empty();
+		setupTilegrid(tileGrid);
+		setTileGridData(tileGrid, dataArray);
+
+
+		/* ********** Inner HELPERS ********** */
+
+		function setupTilegrid(tileGrid) {
+			for (var i = 0; i < SIZE; i++) {
+				appendRow(tileGrid);
+			}
+			
+			function appendRow(tileGrid) {
+				var row = $('<div/>', {
+					class: 'row'
+				});
+				for (var j = 0; j < SIZE; j++) {
+					$('<div/>', {
+					    class: 'col-md-4 gridcell square',
+					    text: 'cell'
+					}).appendTo(row);
+				}
+				$(tileGrid).append(row);
+			}
+		}
+
+		function setTileGridData(tileGrid, array) {
+			var i = 0
+			$(tileGrid).find(".gridcell").each(function() {
+				$(this).text(array[i]);
+				if (array[i] === 0) {
+					$(this).addClass("zerocell");
+				}
+				i++;
+			});
+		}
 	}
-
-
-
 }
